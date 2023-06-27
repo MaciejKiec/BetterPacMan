2c411dae64ed483cdf4c866fdaed567b69258e8d
MaciejKiec <harmonicisland8@gmail.com>
2023-06-27 16:21:36 +0200

    feat: threads management added

--- src/pacman/IDAssigner.java
+++ src/pacman/IDAssigner.java
@@ -1,2 +1,26 @@
-package pacman;public class IDAssigner {
+package pacman;
+
+/**
+ * The IDAssigner class is responsible for assigning unique IDs to objects.
+ */
+public class IDAssigner {
+    private int baseID;
+
+    /**
+     * Constructs an IDAssigner object with the specified base ID.
+     *
+     * @param baseID the base ID from which the assignment starts
+     */
+    public IDAssigner(int baseID) {
+        this.baseID = baseID;
+    }
+
+    /**
+     * Retrieves the next available ID.
+     *
+     * @return the next available ID
+     */
+    public int next() {
+        return baseID++;
+    }
 }
--- src/pacman/PooledThread.java
+++ src/pacman/PooledThread.java
@@ -1,2 +1,44 @@
-package pacman;public class PooledThread {
+package pacman;
+
+/**
+ * The PooledThread class represents a thread that is part of a thread pool.
+ */
+public class PooledThread extends Thread {
+    private static final IDAssigner threadID = new IDAssigner(1);
+    private final ThreadPool pool;
+
+    /**
+     * Constructs a PooledThread object associated with the given thread pool.
+     *
+     * @param pool the thread pool to which this thread belongs
+     */
+    public PooledThread(ThreadPool pool) {
+        super(pool, "PooledThread-" + threadID.next());
+        this.pool = pool;
+    }
+
+    /**
+     * Executes the main logic of the thread.
+     * Continuously retrieves tasks from the thread pool and executes them until interrupted.
+     * Handles exceptions and passes them to the thread pool's uncaught exception handler.
+     */
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+            Runnable task = null;
+            try {
+                task = pool.getTask();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (task == null)
+                return;
+
+            try {
+                task.run();
+            } catch (Throwable t) {
+                pool.uncaughtException(this, t);
+            }
+        }
+    }
 }
